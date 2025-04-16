39a2be108a762789391baf01359b96d60b3b6e0a
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2025-04-16 19:01:52 +0700

    feat: Init

---
+++ src/test/java/ru/steqa/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.steqa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
