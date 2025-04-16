ea4d446d7a75b16a8a6979cc941ba1ed2e3b75be
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2025-04-16 19:08:35 +0700

    feat: Add transaction controller

---
+++ src/main/java/ru/steqa/api/controller/TransactionController.java
@@ -0,0 +1,52 @@
+package ru.steqa.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.steqa.api.schema.transaction.AddTransactionSchema;
+import ru.steqa.api.schema.transaction.ResponseTransactionSchema;
+import ru.steqa.api.schema.transaction.UpdateTransactionSchema;
+import ru.steqa.api.service.transaction.ITransactionService;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+    private final ITransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<ResponseTransactionSchema> addTransaction(@RequestBody AddTransactionSchema request) {
+        ResponseTransactionSchema transaction = transactionService.addTransaction(request);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(transaction.getId()).toUri();
+        return ResponseEntity.created(location).body(transaction);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseTransactionSchema>> getAllTransactions() {
+        return ResponseEntity.ok(transactionService.getTransactions());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseTransactionSchema> getTransactionById(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.getTransactionById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ResponseTransactionSchema> updateTransaction(
+            @RequestBody UpdateTransactionSchema request,
+            @PathVariable Long id
+    ) {
+        ResponseTransactionSchema transaction = transactionService.updateTransaction(request, id);
+        return ResponseEntity.ok(transaction);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
+        transactionService.deleteTransactionById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
